f2702f68288e4e6022f7320a577f4195befc1396
Dafter <dafterplay@mail.ru>
2024-04-02 20:03:18 +0300

    add test_1

---
+++ Lab 2/test_1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from neural_network import Hopfild
+
+"""
+Этот тест демонстрирует, что при запоминании 1 объекта, мы получаем его инверсную копию
+"""
+
+l = Hopfild(4, 100)
+l.remember([np.array([1, 1, -1, -1]).reshape(4, 1)])
+print(l.associations(np.array([1, -1, -1, 1])))
+print(l.associations(np.array([1, 1, -1, -1])))
