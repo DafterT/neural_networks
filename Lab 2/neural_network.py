0d4f434fc024d8a4a6fae5a46e0136b607709092
Dafter <dafterplay@mail.ru>
2024-04-03 10:21:01 +0300

    add energy

--- Lab 2/neural_network.py
+++ Lab 2/neural_network.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -7,6 +8,7 @@ class Hopfild:
         self.N = N  # количество нейронов
         self.K = k  # максимальное количество эпох распознавания сигнала
         self.W = np.zeros((self.N, self.N))  # матрица взаимодействий (весов)
+        self.energy = []
 
     def remember(self, M):  # метод запоминания образов
         for X in M:
@@ -19,7 +21,18 @@ class Hopfild:
             for j in range(self.N):
                 H_i += self.W[i][j] * signal[j]
             signal[i] = self.signum(H_i)
+            self._calc_energy(signal)
         return signal
+
+    def _calc_energy(self, X):
+        self.energy.append(-1 / 2 * (X.T.dot(self.W).dot(X)).flatten())
+
+    def clear_energy(self):
+        self.energy = []
+
+    def print_energy(self):
+        plt.plot(self.energy)
+        plt.show()
 
     def associations(self, signal):  # распознавание образа
         X = signal.copy()  # текущее состояние
@@ -29,6 +42,7 @@ class Hopfild:
 
             if (x_before == X).all():  # выход из цикла, если значения стабилизировались
                 return X
+        print('End by count iterations!')
         return X
 
     def signum(self, a):  # функция активации
