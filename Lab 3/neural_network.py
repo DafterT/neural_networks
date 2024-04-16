8b9b8be44d5d492e1b46eaeee20d5900382eb179
DafterPlay <dafterplay@mail.ru>
2024-04-16 22:24:20 +0300

    update func

--- Lab 3/neural_network.py
+++ Lab 3/neural_network.py
@@ -2,73 +2,65 @@ import numpy as np
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 
+rand = np.random.RandomState(0)
 
 class Kohonen:
-    def __init__(self, M: int, K: int) -> None:
+    def __init__(self, M: int, K: int):
         """
         Initial function
 
         Args:
             M (int): size of input layer
-            K (int): size of layer
+            K (int): size of side of square
         """
         self.M = M
         self.K = K
         self.W = None
 
-    def generate_W(self) -> None:
-        """
-        Generate the weight matrix for Kohonen's self organizing map.
+    def generate_W(self):
+        self.W = rand.uniform(0, 1, (self.K, self.K, self.M)).astype(float)
 
-        The weight matrix is initialized with random values between 0.5 - 1/sqrt(M) and 0.5 + 1/sqrt(M)
-        where M is the size of the input layer.
-        """
-        self.W = np.random.uniform(0.5 - 1 / (self.M ** 0.5), 0.5 + 1 / (self.M ** 0.5) + 1, size=(self.K, self.M))
-    
-    @staticmethod
-    def _eta_function(n, eta_0=0.1, t2=2000):
-        return eta_0 * np.exp(-n / t2)
-    
-    @staticmethod
-    def plot_eta():
-        x = np.linspace(0, 10**4, 1000)
-        plt.plot(x, Kohonen._eta_function(x))
-        plt.show()
-    
-    @staticmethod
-    def _sigma_function(n, sig_0=10, t1=1000):
-        return (sig_0 * np.exp(-n / t1))
-    
-    @staticmethod
-    def plot_sigma():
-        x = np.linspace(0, 10**4, 1000)
-        plt.plot(x, Kohonen._sigma_function(x))
-        plt.show()
-    
-    def _iteration(self, in_x, n):
-        winner_index = np.argmax([distance.euclidean(in_x, i) for i in self.W])
-        size_of_side = int(self.K ** 0.5)
-        winner = (winner_index // size_of_side, winner_index % size_of_side)
-        
-        distance_Gauss = np.zeros((size_of_side, size_of_side))
-        for i in range(size_of_side):
-            for j in range(size_of_side):
-                distance_Gauss[i][j] = np.exp(-distance.euclidean(winner, (i, j)) / (2 * self._sigma_function(n)))
-                
-        dW = np.zeros((self.K, self.M))
-        for i in range(size_of_side):
-            for j in range(size_of_side):
-                dW[i * size_of_side + j] = Kohonen._eta_function(n) * distance_Gauss[i][j] * (in_x - self.W[i * size_of_side + j])
+    def _find_winner(self, in_x):
+        distSq = (np.square(self.W - in_x)).sum(axis=2)
+        return np.unravel_index(np.argmin(distSq, axis=None), distSq.shape)
 
-        self.W = self.W + dW
-            
+    def _iteration(self, in_x, learn_rate, radius_sq, step=3):
+        g, h = self._find_winner(in_x)
+        if radius_sq < 1e-3:
+            self.W[g, h, :] += learn_rate * (in_x - self.W[g, h, :])
+            return
+        for i in range(max(0, g - step), min(self.W.shape[0], g + step)):
+            for j in range(max(0, h - step), min(self.W.shape[1], h + step)):
+
+                dist_sq = np.square(i - g) + np.square(j - h)
+                dist_func = np.exp(-dist_sq / 2 / radius_sq)
+                self.W[i, j, :] += learn_rate * dist_func * (in_x - self.W[i, j, :])
+
+    def calculate(self, dataset, count_iter,
+                  learn_rate_0=.1, lr_decay=.1,
+                  radius_0=1, radius_decay=.1):
+        dataset_copy = dataset.copy()
+        for i in range(count_iter):
+            learn_rate = learn_rate_0 * np.exp(-i * lr_decay)
+            radius_sq = radius_0 * np.exp(-i * radius_decay)
+            rand.shuffle(dataset_copy)
+            if i % 1 == 0:
+                print(round(i / count_iter * 100, 1))
+            for data in dataset_copy:
+                self._iteration(data, learn_rate, radius_sq)
+
 
 def main():
-    kh = Kohonen(7, 4)
-    # Kohonen.plot_eta()
-    # Kohonen.plot_sigma()
+    kh = Kohonen(2, 10)
     kh.generate_W()
-    kh._iteration(np.random.uniform(0, 1, size=(7)), 10000)
+    train_data = np.append(np.random.uniform(0, 1, (5000, 2)), np.random.uniform(0, 1, (5000, 2)), axis=0)
+    plt.scatter(*zip(*train_data))
+    plt.show()
+    plt.scatter(*zip(*kh.W.reshape(1, 100, 2)[0]))
+    plt.show()
+    kh.calculate(train_data, 10)
+    plt.scatter(*zip(*kh.W.reshape(1, 100, 2)[0]))
+    plt.show()
 
 
 if __name__ == '__main__':
