3f6818b12035343ce10a6ded299a7890d37d52af
DafterPlay <dafterplay@mail.ru>
2024-04-17 10:29:39 +0300

    redo example 3

--- Lab 3/example_3.py
+++ Lab 3/example_3.py
@@ -1,33 +1,21 @@
-# %%
-import pandas as pd
 import numpy as np
-from neural_network import Kohonen
 import matplotlib.pyplot as plt
-# %%
-df = pd.read_csv('my_data_norm.csv')
-df.head()
-# %%
-selected_columns = ['ac', 'health', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma']
-df_selected = df[selected_columns]
-numpy_arrays = df_selected.to_numpy()
-numpy_arrays
-# %%
-kh = Kohonen(len(selected_columns), 400)
-kh.generate_W()
-# %%
-kh.calculate(numpy_arrays, 5)
-# %%
-kh.W.shape
-# %%
+from neural_network import Kohonen, rand
+
+kh = Kohonen(3, 10)
+train_data = rand.uniform(0, 1, (3000, 3))
+
 fig, ax = plt.subplots(
-    nrows=2, ncols=4, figsize=(12, 4), 
+    nrows=1, ncols=3, figsize=(12, 3.5), 
     subplot_kw=dict(xticks=[], yticks=[]))
+ax[0].imshow(train_data.reshape(50, 60, 3))
+ax[0].title.set_text('Исходные данные')
 
-# Initialize the SOM randomly to the same state
+kh.generate_W()
+ax[1].imshow(kh.W)
+ax[1].title.set_text('Случайно сгенерированные веса')
 
-for i in range(2):
-    for j in range(4):
-        new_arr = np.expand_dims(kh.W[..., i * 2 + j], axis=-1)
-        new_arr = np.concatenate([new_arr]*3, axis=-1)
-        ax[i][j].imshow(new_arr)
-        ax[i][j].title.set_text(f'{i * 2 + j}')
+kh.calculate(train_data, 10)
+ax[2].imshow(kh.W)
+ax[2].title.set_text('Веса после обучения')
+plt.show()
