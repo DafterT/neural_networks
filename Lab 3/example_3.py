406f4f75dfd03d81742b4788ec8929c694072d34
DafterPlay <dafterplay@mail.ru>
2024-04-17 10:37:13 +0300

    update example 3

--- Lab 3/example_3.py
+++ Lab 3/example_3.py
@@ -6,16 +6,20 @@ kh = Kohonen(3, 10)
 train_data = rand.uniform(0, 1, (3000, 3))
 
 fig, ax = plt.subplots(
-    nrows=1, ncols=3, figsize=(12, 3.5), 
+    nrows=1, ncols=5, figsize=(12, 4), 
     subplot_kw=dict(xticks=[], yticks=[]))
+
 ax[0].imshow(train_data.reshape(50, 60, 3))
 ax[0].title.set_text('Исходные данные')
 
 kh.generate_W()
 ax[1].imshow(kh.W)
-ax[1].title.set_text('Случайно сгенерированные веса')
+ax[1].title.set_text('Случайные веса')
 
-kh.calculate(train_data, 10)
-ax[2].imshow(kh.W)
-ax[2].title.set_text('Веса после обучения')
+for i, epochs in enumerate([1, 4, 5], start=2):
+    kh.calculate(train_data, epochs)
+    ax[i].imshow(kh.W)
+    ax[i].title.set_text(f'Веса после обучения {[1, 5, 10][i - 2]}')
+
+plt.tight_layout()
 plt.show()
